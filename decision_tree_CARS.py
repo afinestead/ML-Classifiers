113e30ae963f81fff057c19053ba86a6708f3d79
pinzhu <32945571+pinzhu@users.noreply.github.com>
2018-03-07 14:21:20 -0800

    Added more cmd line features, fixed warnings. Should be final commit

--- decision_tree_CARS.py
+++ decision_tree_CARS.py
@@ -5,111 +5,113 @@ import numpy as np
 samplefilepath = 'Car.csv'
 PCT_TRAIN = 0.90
 
-#Entropy value for a target is being returned to the calling function
-def entropy_for_a_target(frequency_of_tdata, length_of_data):
-    ent= (-frequency_of_tdata/length_of_data)*math.log(frequency_of_tdata/length_of_data,2)
-    return ent
+#Probability is calculated, it returns the freq/length of data
+def chance(val, division):
+    length = len(division)
+    freq = division.count(val)
+    return freq/length
+
 
 #Function to calculate the entropy, returns the entropy
-def entropy(data):
-    targets = [row[0] for row in data]
-    frequency_of_targets = {x: targets.count(x) for x in set(targets)}
+def cal_entropy(data):
+    goal = [row[0] for row in data]
+    targets_freq = {i: goal.count(i) for i in set(goal)}
     #after finding the frequency of targets, entropy_by_target is found by invoking entropy_for_a_target function
-    entropy_by_target = {x: entropy_for_a_target(frequency, len(data)) for x, frequency in frequency_of_targets.items()}
+    entropy_target = {i: entropy_target_val(freq, len(data)) for i, freq in targets_freq.items()}
     # the entropy value is returned
-    return sum(entropy_by_target.values())
+    return sum(entropy_target.values())
 
-#Probability is calculated, it returns the freq/length of data
-def get_probability(target_value, distribution):
-    length_of_data = len(distribution)
-    frequency_of_target = distribution.count(target_value)
-    return frequency_of_target/length_of_data
+#Entropy value for a target is being returned to the calling function
+def entropy_target_val(freq, data):
+    val = (-freq/data)*math.log(freq/data,2)
+    return val
+
 
 #Function to calculate the information gain, returns the gain value
-def informationgain(data, attribute_index):
+def info_Gain(data, index):
     #entropy function is called
-    entropy_value = entropy(data)
-    attribute_values = [x[attribute_index] for x in data]
+    entropy_val = cal_entropy(data) # entropy value
+    attr_val = [x[index] for x in data] # attribute_values
     #unique attribute values is found
-    unique_attribute_values = set(attribute_values)
+    attr = set(attr_val)
     gain = 0
-    for attribute_value in unique_attribute_values:
+    for attribute_value in attr:
         #filtered data gets calculated
-        filtered_data = [x for x in data if x[attribute_index] == attribute_value]
+        new_data = [x for x in data if x[index] == attribute_value]
         #Probabaility function is called
-        prob = get_probability(attribute_value, attribute_values)
-        entropy_for_value = entropy(filtered_data)
-        gain += (prob * entropy_for_value)
+        probability = chance(attribute_value, attr_val)
+        entropy = cal_entropy(new_data)
+        gain += (probability * entropy)
         #returns the gain value
-    return entropy_value - gain
+    return entropy_val - gain
+
 
 # Function to choose the best attribute, it returns the maximum gain attribute
-def choose_attribute(data):
+def best_attribute(data):
     max_gain = 0
     max_gain_attribute = -1
     total_attributes = len(data[0]) - 1
     for x in range(total_attributes):
         attribute_index = x + 1
         #informationgain function is called to get the gain values of attributes
-        gain = informationgain(data, attribute_index)
+        gain = info_Gain(data, attribute_index)
         if gain > max_gain:
             max_gain = gain
             max_gain_attribute = attribute_index
     return max_gain_attribute
 
 #Function to build the decision tree, it returns a tree
-def build_tree(data, title):
-    total_attributes = len(data[0]) - 1
-    if total_attributes == 0:
+def build_decision_tree(data, title):
+    total = len(data[0]) - 1
+    if total == 0:
         return
-    target_values = set([x[0] for x in data])
-    if len(target_values) == 1:
-        return target_values.pop()
+    goal = set([i[0] for i in data])
+    if len(goal) == 1:
+        return goal.pop()
     #The best attribute is choosen through the choose_attribute function
-    chosen_attribute_index = choose_attribute(data)
-    tree = {title[chosen_attribute_index]: {}}
-    attribute_values = set([x[chosen_attribute_index] for x in data])
-    for value in attribute_values:
-        filtered_data = [x for x in data if x[chosen_attribute_index] == value]
-        subtree = build_tree(filtered_data, title)
-        tree[title[chosen_attribute_index]][value] = subtree
+    choose = best_attribute(data)
+    tree = {title[choose]: {}}
+    attr_val = set([i[choose] for i in data])
+    for value in attr_val:
+        new_data = [i for i in data if i[choose] == value]
+        sub_tree = build_decision_tree(new_data, title)
+        tree[title[choose]][value] = sub_tree
     return tree
 
+#Function to test the decision tree and return the percentage of accuracy
+def test_tree(data, title, goal, tree):
+    targets = [row[0] for row in data]
+    accurate = 0
+    pred = []
+    #every instance in the test data gets checked
+    for index, row in enumerate(data):
+        #the value from get_result_from_decision_tree is stored in result
+        result_val = result(tree, row, title, goal)
+
+        if result_val == targets[index]:
+        #if the value in result equals the target value in the set it increments the no_of_correct_answers
+            accurate +=1
+            pred.append(targets[index])
+    return accurate, len(data), pred
+
 #Function to get the result from decision tree, it returns a tree
-def get_result_from_decision_tree(tree, row, title, unique_targets):
+def result(tree, row, title, goal):
     if len(row) != len(title):
         print("R", row)
         print("T", title)
         print("PROBLEM")
 
     while True:
-        root_key = list(tree.keys())[0]
-        root_index = title.index(root_key)
-        value = row[root_index]
-        if root_key not in tree:
+        key = list(tree.keys())[0]
+        index = title.index(key)
+        val = row[index]
+        if key not in tree:
             return None
-        if value not in tree[root_key]:
+        if val not in tree[key]:
             return None
-        tree = tree[root_key][value]
-        if tree in list(unique_targets):
+        tree = tree[key][val]
+        if tree in list(goal):
             return tree
-
-#Function to test the decision tree and return the percentage of accuracy
-def test_decision_tree(test_data, title, unique_targets, tree):
-    targets = [row[0] for row in test_data]
-    no_of_correct_answers = 0
-    pred = []
-    #every instance in the test data gets checked
-    for index, row in enumerate(test_data):
-        #the value from get_result_from_decision_tree is stored in result
-        result = get_result_from_decision_tree(tree, row, title, unique_targets)
-
-        if result == targets[index]:
-        #if the value in result equals the target value in the set it increments the no_of_correct_answers
-            no_of_correct_answers+=1
-            pred.append(targets[index])
-    #return no_of_correct_answers/len(test_data) * 100
-    return no_of_correct_answers, len(test_data), pred
 
 def run_classifier(PCT_TRAIN, verbose=True):
     #Function to read the dataset from csv file
@@ -126,9 +128,9 @@ def run_classifier(PCT_TRAIN, verbose=True):
       testing_row_indices = np.random.choice(len(data), int(len(data) * (1-PCT_TRAIN)), replace=True)
       X_test = [data[i] for i in testing_row_indices]
       #build_tree function is called
-      tree = build_tree(X_train, title)
+      tree = build_decision_tree(X_train, title)
       #accuracy of the test data is calculated through the test_decision_tree function
-      accurate, total, pred = test_decision_tree(X_test, title, unique_targets, tree)
+      accurate, total, pred = test_tree(X_test, title, unique_targets, tree)
 
 
       col1 = {'vhigh': 0, 'high': 1, 'med': 2, 'low':3}
@@ -140,13 +142,13 @@ def run_classifier(PCT_TRAIN, verbose=True):
       listn = []
 
       if verbose:
-        for i in range(0, len(pred)):
-            listn = [col1[X_test[i][1]], col2[X_test[i][2]], col3[X_test[i][3]], col4[X_test[i][4]], col5[X_test[i][5]],
-                    col6[X_test[i][6]]]
-            if pred[i] == X_test[i][0]:
-                print ("(CORRECT) ", listn," is predicted to be " + pred[i] + ". In reality, it is " + X_test[i][0])
-            else:
-                print("(INCORRECT) ", listn," is predicted to be " + pred[i] + ". In reality, it is " + X_test[i][0])
+          for i in range(0, len(pred)):
+              listn = [col1[X_test[i][1]], col2[X_test[i][2]], col3[X_test[i][3]], col4[X_test[i][4]], col5[X_test[i][5]],
+                        col6[X_test[i][6]]]
+              if pred[i] == X_test[i][0]:
+                  print ("(CORRECT) ", listn," is predicted to be " + pred[i] + ". In reality, it is " + X_test[i][0])
+              else:
+                  print("(INCORRECT) ", listn," is predicted to be " + pred[i] + ". In reality, it is " + X_test[i][0])
 
       #print("accurate prediciton: ", accurate, ", total prediction:", total)
       return accurate, total
